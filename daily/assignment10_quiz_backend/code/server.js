039d9340bb46dc835b67912da2d3c7db5806e7f0
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2019-09-30 08:52:11 +0530

    daily assignment ideas

--- daily/assignment10_quiz_backend/code/server.js
+++ daily/assignment10_quiz_backend/code/server.js
@@ -5,10 +5,16 @@ app.use(express.json());
 
 const http = require('http').Server(app);
 var questions = [  ];
+var questionCounter = 1;
 app.post('/questions', function(req, res) {
     var question = req.body;
-    questions.push(question);
+    question.questionId = questionCounter;
+    questionCounter ++;
+    questions.push(question); 
+
+    res.json(question);
 });
+
 app.use(express.static("public"));
 const server = http.listen(3000, function() {
     console.log('listening on *:3000');
